05b62c246c75acc06a361f0789b8588a343f6cd9
toushik-bit <57601070+toushik-bit@users.noreply.github.com>
2019-11-11 09:28:22 +0600

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice.pkg1;
+
+/**
+ *
+ * @author Aspire
+ */
+public class Car {
+   public void m1() {
+      System.out.println("car 1");
+   }
+
+   public void m2() {
+      System.out.println("car 2");
+   }
+
+   public String toString() {
+      return "vroom";
+   }
+}
